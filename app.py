ac4148ff604f8080eed3a616441555a40e49383d
Remy.Jacquemin <remy.jacquemin@ccq.org>
2025-10-31 08:58:03 -0400

    Ajout de jupythery

--- app.py
+++ app.py
@@ -1,4 +1,24 @@
 import requests
-response = requests.get('https://www.google.com', verify=False)
-if response.status_code == 200:
-    print(response.text)
+
+
+class Person:
+    
+    def __init__(self, name):
+        self.name = name
+        self.counter = 0
+
+    def greet(self):
+        return f"Hello, {self.name}!"
+    
+
+
+# response = requests.get('https://www.google.com', verify=False)
+# if response.status_code == 200:
+#     print(response.text)
+
+person = Person("Alice")
+person.age = 30
+print(person.name)
+
+person.counter += 1
+print(person.counter)
